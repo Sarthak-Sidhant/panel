3c7c6f6745b5682533641750033c4e61174f0ad1
Sarthak-Sidhant <sarthaksidhantisopolice@gmail.com>
2023-05-01 23:51:26 +0530

    scriputeate

--- script.js
+++ script.js
@@ -37,3 +37,68 @@ $(".window .content").mousedown(function(event) {
 $(document).mousemove(function(event) {
 	if (resizing) {
 		var newWidth = startWidth + event.clientX - resizeItem.offset().left
+		var newHeight = startHeight + event.clientY - resizeItem.offset().top;
+		if (newWidth < minWidth) {
+			newWidth = minWidth;
+		}
+		if (newHeight < minHeight) {
+			newHeight = minHeight;
+		}
+		resizeItem.width(newWidth);
+		resizeItem.height(newHeight);
+	}
+});
+$(document).mouseup(function() {
+	resizing = false;
+});
+
+// Minimizing and restoring
+$(".window .titlebar .buttons a.minimize").click(function() {
+	var window = $(this).parents(".window");
+	window.addClass("minimized");
+});
+$("#taskbar li").click(function() {
+	var window = $("#" + $(this).data("window-id"));
+	window.removeClass("minimized");
+	window.addClass("active");
+	window.siblings(".window").removeClass("active");
+});
+
+// Closing
+$(".window .titlebar .buttons a.close").click(function() {
+	$(this).parents(".window").remove();
+	$("#taskbar li[data-window-id='" + $(this).parents(".window").attr("id") + "']").remove();
+});
+
+// Fullscreen
+$(".window .titlebar .buttons a.fullscreen").click(function() {
+	var window = $(this).parents(".window");
+	if (!window.hasClass("fullscreen")) {
+		window.addClass("fullscreen");
+		$(this).html("&#x2198;");
+	} else {
+		window.removeClass("fullscreen");
+		$(this).html("&#x2196;");
+	}
+});
+
+// Creating new windows
+$("#compiler-button").click(function() {
+	createWindow("compiler.html", "Compiler");
+});
+$("#calculator-button").click(function() {
+	createWindow("calculator.html", "Calculator");
+});
+$("#fitness-calculator-button").click(function() {
+	createWindow("fitness-calculator.html", "Fitness Calculator");
+});
+$("#notes-button").click(function() {
+	createWindow("notes.html", "Notes");
+});
+$("#todo-button").click(function() {
+	createWindow("todo.html", "To-Do List");
+});
+$("#calendar-button").click(function() {
+	createWindow("calendar.html", "Calendar");
+});
+
