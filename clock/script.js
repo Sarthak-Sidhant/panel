61e91730d6af5fb49eb31e5de81df590a1245ac9
Sarthak-Sidhant <sarthaksidhantisopolice@gmail.com>
2023-05-03 23:49:47 +0530

    it actually was wrong

--- clock/script.js
+++ clock/script.js
@@ -10,91 +10,88 @@ const yearElement = document.getElementById('year');
 const timerHoursElement = document.getElementById('timer-hours');
 const timerMinutesElement = document.getElementById('timer-minutes');
 const timerSecondsElement = document.getElementById('timer-seconds');
-const startTimerButton = document.getElementById('start-timer');
-const pauseTimerButton = document.getElementById('pause-timer');
-const stopTimerButton = document.getElementById('stop-timer');
+const startTimerButton = document.getElementById('start-timer-button');
+const pauseTimerButton = document.getElementById('pause-timer-button');
+const stopTimerButton = document.getElementById('stop-timer-button');
 
 // Get the stopwatch elements
-const stopwatchHoursElement = document.getElementById('sw-hours');
-const stopwatchMinutesElement = document.getElementById('sw-minutes');
-const stopwatchSecondsElement = document.getElementById('sw-seconds');
-const stopwatchMillisecondsElement = document.getElementById('sw-milliseconds');
-const startStopwatchButton = document.getElementById('start-stopwatch');
-const pauseStopwatchButton = document.getElementById('pause-stopwatch');
-const resetStopwatchButton = document.getElementById('reset-stopwatch');
+const stopwatchElement = document.getElementById('stopwatch');
+const startStopwatchButton = document.getElementById('start-stopwatch-button');
+const pauseStopwatchButton = document.getElementById('pause-stopwatch-button');
+const resetStopwatchButton = document.getElementById('reset-stopwatch-button');
 
-// Get the alarms elements
-const alarmInputElement = document.getElementById('alarm-time');
-const setAlarmButton = document.getElementById('set-alarm');
-const stopAlarmButton = document.getElementById('stop-alarm');
+// Get the alarm elements
+const alarmHoursElement = document.getElementById('alarm-hours');
+const alarmMinutesElement = document.getElementById('alarm-minutes');
+const setAlarmButton = document.getElementById('set-alarm-button');
+const cancelAlarmButton = document.getElementById('cancel-alarm-button');
 
-// Clock function
-function clock() {
-  const now = new// Get the current date and time
+// Display the current date and time
+displayDateTime();
+
+// Update the clock every second
+setInterval(displayDateTime, 1000);
+
+// Set up the timer event listeners
+startTimerButton.addEventListener('click', startTimer);
+pauseTimerButton.addEventListener('click', pauseTimer);
+stopTimerButton.addEventListener('click', stopTimer);
+
+// Set up the stopwatch event listeners
+startStopwatchButton.addEventListener('click', startStopwatch);
+pauseStopwatchButton.addEventListener('click', pauseStopwatch);
+resetStopwatchButton.addEventListener('click', resetStopwatch);
+
+// Set up the alarm event listeners
+setAlarmButton.addEventListener('click', setAlarm);
+cancelAlarmButton.addEventListener('click', cancelAlarm);
+
+// Clock and date functions
+function displayDateTime() {
+  // Get the current date and time
   const currentDateTime = new Date();
-  
+
   // Display the current time
   hoursElement.textContent = currentDateTime.getHours().toString().padStart(2, '0');
   minutesElement.textContent = currentDateTime.getMinutes().toString().padStart(2, '0');
   secondsElement.textContent = currentDateTime.getSeconds().toString().padStart(2, '0');
-  
+
   // Display the current date
   dayElement.textContent = currentDateTime.getDate().toString().padStart(2, '0');
   monthElement.textContent = (currentDateTime.getMonth() + 1).toString().padStart(2, '0');
   yearElement.textContent = currentDateTime.getFullYear();
-  }
-  
-  // Timer function
-  let timerId;
-  let timerDuration;
-  
-  function startTimer() {
+}
+
+// Timer function
+let timerId;
+let timerDuration;
+
+function startTimer() {
   const hours = Number(timerHoursElement.value);
   const minutes = Number(timerMinutesElement.value);
   const seconds = Number(timerSecondsElement.value);
-  
+
   // Calculate the timer duration in milliseconds
   timerDuration = ((hours * 60 * 60) + (minutes * 60) + seconds) * 1000;
-  
+
   // Start the timer
   timerId = setInterval(updateTimer, 1000);
-  
+
   // Disable the timer inputs and the start button
   timerHoursElement.disabled = true;
   timerMinutesElement.disabled = true;
   timerSecondsElement.disabled = true;
   startTimerButton.disabled = true;
-  }
-  
-  function pauseTimer() {
+}
+
+function pauseTimer() {
   // Pause the timer
   clearInterval(timerId);
-  
+
   // Enable the stop button and disable the pause button
   stopTimerButton.disabled = false;
   pauseTimerButton.disabled = true;
-  }
-  
-  function stopTimer() {
-  // Stop the timer and reset the inputs
-  clearInterval(timerId);
-  timerHoursElement.value = '';
-  timerMinutesElement.value = '';
-  timerSecondsElement.value = '';
-  
-  // Enable the inputs and the start button
-  timerHoursElement.disabled = false;
-  timerMinutesElement.disabled = false;
-  timerSecondsElement.disabled = false;
-  startTimerButton.disabled = false;
-  }
-  
-  function updateTimer() {
-  // Calculate the remaining time
-  timerDuration -= 1000;
-  
-  if (timerDuration <= 0) {
-  // Stop the timer and play a sound
-  clearInterval(timerId);
-  const alarmSound = new Audio('alarm.mp3');
-  alarmSound.play();
+}
+
+function stopTimer() {
+  // Stop the timer and reset
