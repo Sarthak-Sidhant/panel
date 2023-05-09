0e54c0c5ddd6e7c13846c122d46a56f1205d6904
Sarthak-Sidhant <sarthaksidhantisopolice@gmail.com>
2023-05-09 23:56:54 +0530

    handle_data_available event

--- recorder/script.js
+++ recorder/script.js
@@ -5,3 +5,11 @@ const stopButton = document.getElementById("stop");
 let mediaStream;
 let mediaRecorder;
 let recordedChunks = [];
+
+const handleDataAvailable = (event) => {
+    if (event.data.size > 0) {
+      recordedChunks.push(event.data);
+    }
+  };
+  
+  
