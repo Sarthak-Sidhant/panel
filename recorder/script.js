5107986234359932c14a23955c88f160fb112f39
Sarthak-Sidhant <sarthaksidhantisopolice@gmail.com>
2023-05-09 23:56:27 +0530

    let my life be a disappointment

--- recorder/script.js
+++ recorder/script.js
@@ -1,3 +1,7 @@
 const videoElement = document.getElementById("video");
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
+
+let mediaStream;
+let mediaRecorder;
+let recordedChunks = [];
